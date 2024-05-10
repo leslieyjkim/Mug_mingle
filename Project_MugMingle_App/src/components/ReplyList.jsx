3b0f563baed4e7553f7335da28ad75fd2ac6b851
yoonjookim <yjblossom@gmail.com>
2024-05-10 13:30:11 -0400

    Separate component#4. ReplyListItem.jsx from the #3.ReplyList.jsx

--- Project_MugMingle_App/src/components/ReplyList.jsx
+++ Project_MugMingle_App/src/components/ReplyList.jsx
@@ -1,18 +1,12 @@
+import ReplyListItem from "./ReplyListItem";
+
 export default function ReplyList(props) {
     return (
         <section className='ReplyList'>
             <h1>X replies to this question</h1>
             <ul>
-                <li className='ReplyListItem'>
-                    <img src="" alt="" />
-                    <p>Name</p>
-                    <p>Reply</p>
-                </li>
-                <li className='ReplyListItem'>
-                    <img src="" alt="" />
-                    <p>Name</p>
-                    <p>Reply</p>
-                </li>
+                <ReplyListItem />
+                <ReplyListItem />
             </ul>
         </section>
     )
---
+++ Project_MugMingle_App/src/components/ReplyListItem.jsx
@@ -0,0 +1,9 @@
+export default function ReplyListItem(props) {
+    return (
+        <li className='ReplyListItem'>
+            <img src="" alt="" />
+            <p>Name</p>
+            <p>Reply</p>
+        </li>
+    )
+}
