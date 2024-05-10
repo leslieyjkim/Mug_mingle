169cf66f8ee1b154da1c2bbd926f0383d515d6ea
yoonjookim <yjblossom@gmail.com>
2024-05-10 13:26:32 -0400

    Separate component#3. ReplyList.jsx

--- Project_MugMingle_App/src/App.jsx
+++ Project_MugMingle_App/src/App.jsx
@@ -1,5 +1,6 @@
 import Header from "./components/Header";
 import Question from "./components/Question";
+import ReplyList from "./components/ReplyList";
 
 import './App.css'
 
@@ -7,24 +8,15 @@ import './App.css'
 function App() {
   return (
     <>
-    <Header />
-    <Question />
-    <main>
-      <section className='ReplyList'>
-        <h1>X replies to this question</h1>
-        <ul>
-          <li className='ReplyListItem'>
-            <img src="" alt=""/>
-            <p>Name</p>
-            <p>Reply</p>
-          </li>
-        </ul>
-      </section>
-      <form className='ReplyForm'>
-        <input type="text" name="reply" placeholder='Enter your reply' />
-        <button>Add</button>
-      </form>
-    </main>
+      <Header />
+      <main>
+        <Question />
+        <ReplyList />
+        <form className='ReplyForm'>
+          <input type="text" name="reply" placeholder='Enter your reply' />
+          <button>Add</button>
+        </form>
+      </main>
     </>
   )
 }
---
+++ Project_MugMingle_App/src/components/ReplyList.jsx
@@ -0,0 +1,19 @@
+export default function ReplyList(props) {
+    return (
+        <section className='ReplyList'>
+            <h1>X replies to this question</h1>
+            <ul>
+                <li className='ReplyListItem'>
+                    <img src="" alt="" />
+                    <p>Name</p>
+                    <p>Reply</p>
+                </li>
+                <li className='ReplyListItem'>
+                    <img src="" alt="" />
+                    <p>Name</p>
+                    <p>Reply</p>
+                </li>
+            </ul>
+        </section>
+    )
+}
