9a72834735a093cc19660be0635064f8923c98a3
yoonjookim <yjblossom@gmail.com>
2024-05-11 11:09:53 -0400

    Implement component of 'Question.jsx'-part1

--- Project_MugMingle_App/src/App.jsx
+++ Project_MugMingle_App/src/App.jsx
@@ -15,11 +15,27 @@ function App() {
   const [users, serUsers] = useState(usersObj); //create 'state' for each to access those data things.
   const [post, setPost] = useState(forumPost); //create 'state' for each to access those data things.
 
+
+  const getQuestionInformation = () => {       //create function of 'getQuestionInformation' which returns at the end object containing 'authorName, authorProfile, question'.
+    const {authorId, question} = post          //I'm gonna abstract authorId & question from the postData in data.
+
+    const authorName = users[authorId].name;                 //And if I want to get the 'authorName', this will be 'users' at the position of the authorId(in postData) & and with that authorId, I want to know their name(in userData).
+    const authorProfile = users[authorId].profile_url;
+
+    return {
+      authorName, 
+      authorProfile, 
+      question,
+    };
+  };
+
+  const questionInfo = getQuestionInformation()  //callback function 
+
   return (
     <>
       <Header />
       <main>
-        <Question />
+        <Question { ...questionInfo }/>  
         <ReplyList />
         <ReplyForm onSubmit="" /> 
       </main>
