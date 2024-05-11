2ed02906406db63016f8078e29b386771b55bfae
yoonjookim <yjblossom@gmail.com>
2024-05-11 00:44:08 -0400

    Import data / create 'state' for each data

--- Project_MugMingle_App/src/App.jsx
+++ Project_MugMingle_App/src/App.jsx
@@ -1,12 +1,20 @@
+import { useState } from "react";
+
 import Header from "./components/Header";
 import Question from "./components/Question";
 import ReplyList from "./components/ReplyList";
 import ReplyForm from "./components/ReplyForm";
 
+import { usersObj } from "./data/userData";
+import { forumPost } from "./data/postData"; 
+
 import './App.css'
 
 
 function App() {
+  const [users, serUsers] = useState(usersObj); //create 'state' for each to access those data things.
+  const [post, setPost] = useState(forumPost); //create 'state' for each to access those data things.
+
   return (
     <>
       <Header />
